9bc039821c45f68ceacf13b0f35f618c6f69ad96
xinxucanada <111302670+xinxucanada@users.noreply.github.com>
2022-08-23 00:42:33 -0400

    etape 1

---
+++ XinXu_labo1.py
@@ -0,0 +1,15 @@
+# Xin Xu #2194517
+
+class Des:
+    def __init__(self, nb_faces, valeur1 = 0, valeur2 = 0) -> None:
+        self.nb_faces = nb_faces
+        self.de_1 = valeur1
+        self.de_2 = valeur2
+
+
+d = Des(6,2,3)
+print(d.nb_faces)
+print(d.de_1)
+print(d.de_2)
+
+
