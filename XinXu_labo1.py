f04da0bd136d13ce6a548c050ffe1c71e4909276
xinxucanada <111302670+xinxucanada@users.noreply.github.com>
2022-08-23 00:50:57 -0400

    etape 2

--- XinXu_labo1.py
+++ XinXu_labo1.py
@@ -1,15 +1,31 @@
 # Xin Xu #2194517
 
+import random
+
 class Des:
     def __init__(self, nb_faces, valeur1 = 0, valeur2 = 0) -> None:
         self.nb_faces = nb_faces
         self.de_1 = valeur1
         self.de_2 = valeur2
 
+    def brasser(self):
+        self.de_1 = random.randint(1, self.nb_faces)
+        self.de_2 = random.randint(1, self.nb_faces)
 
-d = Des(6,2,3)
+
+
+
+
+
+
+d = Des(24)
 print(d.nb_faces)
 print(d.de_1)
 print(d.de_2)
+
+for i in range(30):
+    d.brasser()
+    print(d.de_1, end = "\t")
+    print(d.de_2)
 
 
