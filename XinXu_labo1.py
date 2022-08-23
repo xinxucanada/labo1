1f8d0801b6fecc9cd77bab7aa4faec30048e098f
xinxucanada <111302670+xinxucanada@users.noreply.github.com>
2022-08-23 01:35:57 -0400

    etape4

--- XinXu_labo1.py
+++ XinXu_labo1.py
@@ -18,12 +18,46 @@ class Des:
     def comparer(self, d2):
         return self.total() > d2.total()
 
+    def __str__(self):
+        return f"nombre de faces({self.nb_faces})\tdé_1 ({self.de_1})\tdé2 ({self.de_2})"
+
+nbFaces = int(input("Entrez le nombre de faces voulu(6 à 24): "))
+valeur1 = int(input(f"Entrez l'état du dé_1(1 à {nbFaces}): "))
+valeur2 = int(input(f"Entrez l'état du dé_2(1 à {nbFaces}): "))
+de_utilisateur = Des(nbFaces, valeur1, valeur2)
+de_sys = Des(nbFaces)
+
+def menu(d_u, d_s):
+    condition = True
+    while condition:
+        choix = input("1. Brasser les dés #(affiche le résultat des deux paires de dés)\n"+
+        "2. Comparer les dés #(indique si la paire de l'utilisateur est supérieure ou non)\n"+
+        "3. Sortir #(sort de l'exécution du programme)\n")
+        if choix == "3":
+            condition = False
+        elif choix == "1":
+            d_u.brasser()
+            d_s.brasser()
+            print(f"L'état des dés de l'utilisateur: {d_u}")
+            print(f"L'état des dés du système: {d_s}")
+        elif choix == "2":
+            if d_u.comparer(d_s):
+                print("La paire de l'utilisateur est supérieure que la paire du système. ")
+            else:
+                print("La paire de l'utilisateur n'est pas supérieure que la paire du système. ")
+        else:
+            print("Votre choix est invalide, veuillez choisir 1 à 3 ")
+
+
+menu(de_utilisateur,de_sys)
 
 
 
 
 
 
+
+"""
 d = Des(24)
 print(d.nb_faces)
 print(d.de_1)
@@ -34,5 +68,5 @@ for i in range(30):
     d.brasser()
     print(d.de_1, end = "\t")
     print(d.de_2)
-
+"""
 
